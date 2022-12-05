460c5c2967ed5342f3532dd514dc42e621ec1f79
Alberto Polanco <prmus10@gmail.com>
2022-12-05 15:33:39 -0600

    userPorts.js created and filled with all the existing ports.

--- src/infrastructure/ports/userPorts.js
+++ src/infrastructure/ports/userPorts.js
@@ -1,8 +1,10 @@
-import userPortRegisterMock from "../../testing/infrastructure/ports/userPortRegisterUserMock.json"
+//import userPortRegisterMock from "../../testing/infrastructure/ports/userPortResponsesMock.json"
+
+const baseURL = "http://localhost:8050/api/User"
 
 export const userRegisterUser = (username, email, password) => {
     //return userPortRegisterMock;
-    return fetch("http://localhost:8050/api/User/Register", {
+    return fetch(`${baseURL}/Register`, {
         method: "POST",
         body: JSON.stringify({
             "username": username,
@@ -10,7 +12,45 @@ export const userRegisterUser = (username, email, password) => {
             "password": password
         })
     }).then(response => {
+        console.log(response)
         console.log(response.status)
         return response.json()
     })
+}
+
+export const userAddWatchLater = (userId, recipeId) => {
+    return fetch(`${baseURL}/WatchLater`, {
+        method: "POST",
+        body: JSON.stringify({
+            "userId": userId,
+            "recipeId": recipeId
+        })
+    }).then(response => {
+        console.log(response)
+        console.log(response.status)
+        return response.json()
+    })
+}
+
+export const userGetUser = (userId) => {
+  return fetch(`${baseURL}/${userId}`).then(response => {
+      console.log(response)
+      console.log(response.status)
+      return response.json()
+  })
+}
+
+export const userEditUser = (userId, userNewEmail, userNewPassword, userPassword) => {
+  return fetch(`${baseURL}/${userId}`, {
+      method: "PUT",
+      body: JSON.stringify({
+          "NewEmail" : userNewEmail,
+          "NewPassword" : userNewPassword,
+          "Password" : userPassword
+      })
+  }).then(response => {
+      console.log(response)
+      console.log(response.status)
+      return response.json()
+  })
 }
