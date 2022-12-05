a63777b39f4e50db260152ad873c24c45d85c0e8
Alexandra Oreza <A18016321@alumnos.uady.mx>
2022-12-05 12:45:50 -0600

    style changes to searchbar

--- src/infrastructure/UI/search/component/Searchbar.jsx
+++ src/infrastructure/UI/search/component/Searchbar.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { TextField, Autocomplete } from '@mui/material';
+import { TextField, Autocomplete, InputAdornment } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 //TODO: hacer que pueda distinguir entre busqueda de ingrediente y de titulo de receta
 import { Ingredients } from '../../../../testing/data/Ingredients';
 /* import { Recipes } from '../../../../testing/data/Recipes'; */
+import '../../CSS/Searchbar.style.css';
 
 export default function Searchbar() {
     return (
@@ -10,13 +12,21 @@ export default function Searchbar() {
             freeSolo
             multiple
             limitTags={2}
-            id="multiple-limit-tags"
+            id="search-tags"
             options={Ingredients}
             getOptionLabel={(option) => option.Name}
             renderInput={(params) => (
-                <TextField {...params} label="Search" placeholder="Search" />
+                    <TextField {...params} label="Search" placeholder="Search" 
+                        InputProps={{
+                        ...params.InputProps,
+                        startAdornment: (<InputAdornment position="start"> <SearchIcon />
+                        </InputAdornment>)}} />
             )}
-            sx={{ width: '20vw' }}
-        />
+            sx={{ width: '25vw' }}
+        >
+            <SearchIcon />
+        </Autocomplete>
+
+
     );
 }
